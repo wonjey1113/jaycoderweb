282531ce8dc6f7440d897089adee537a9522a358
wonjey1113 <wonjey@hanmail.net>
2020-11-16 15:40:27 +0900

    layout 구성

---
+++ src/main/java/com/jaycoder/web/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.jaycoder.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+		@GetMapping("/list")
+		public String list(Model model) {
+				return "board/list";
+		}
+}
